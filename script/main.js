1326f0facb2be01f89baef1f5ec14add3e3957c1
fumiya0128 <60463038+fumiya0128@users.noreply.github.com>
2020-08-08 12:30:36 +0900

    大幅変更
    共通シード、リトライ、ルール説明等追加

--- script/main.js
+++ script/main.js
@@ -1,34 +1,44 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 function main(param) {
-    var random = param.random;
+    //var random = param.random;// 乱数生成器
     var scene = new g.Scene({
         game: g.game,
-        assetIds: ["font16_1", "glyph_area_16"]
+        assetIds: ["font16_1", "glyph_area_16","rule"]
     });
-    var frameCount = 0;//累計フレーム
-    var frameCount2 = 0;
-    g.game.vars.gameState = {//スコア初期値
-        score: 0
-    };
-    var gameTimeLimit = 50; //秒数
-    var atumar = 1;//アツマール判定
-    scene.message.add(function (msg) {
-        if (msg.data && msg.data.type === "start" && msg.data.parameters && msg.data.parameters.totalTimeLimit) {
-            gameTimeLimit = msg.data.parameters.totalTimeLimit - 10;//新市場用秒数
-            atumar = 0;
-        }
-    });
+    var time = 50; // 制限時間
+    var ruletime=5;//説明時間
+    var timeall=1;//総合経過フレーム計算用(g.game.age)
+    if (param.sessionParameter.totalTimeLimit) {
+        time = param.sessionParameter.totalTimeLimit-ruletime-5; // セッションパラメータで制限時間が指定されたらその値を使用します
+    }
+    // 市場コンテンツのランキングモードでは、g.game.vars.gameState.score の値をスコアとして扱います
+    g.game.vars.gameState = { score: 0 , playThreshold: 1};
     var playersize = 16;//自機サイズ
     scene.loaded.add(function () {
+        // ここからゲーム内容を記述します
         var haikei = new g.FilledRect({
             scene: scene,
             cssColor: "white",
             width: g.game.width,
             height: g.game.height,
+            opacity:0.9,
         });
-        if (atumar == 1) {
-            scene.append(haikei);
+        if (param.isAtsumaru) {
+            haikei.opacity=1;
         }
+        scene.append(haikei);
+        //ルール表示
+        var rule = new g.Sprite({
+            scene: scene,
+            src: scene.assets["rule"],
+            opacity:1,
+            x:-320,
+            y:-180,
+            scaleX:0.3,
+            scaleY:0.3,
+        });
+        
         var glyph = JSON.parse(scene.assets["glyph_area_16"].data);
         var font = new g.BitmapFont({//フォント指定
             src: scene.assets["font16_1"],
@@ -40,8 +50,8 @@ function main(param) {
             scene: scene,
             font: font,
             fontSize: 16,
-            text: "" + g.game.vars.gameState.score,
-            x: g.game.width - 16 * 5,  // 右端に10文字くらい表示できるように配置
+            text: "0000" + g.game.vars.gameState.score,
+            x: g.game.width - 16 * 5,
             y: 0
         });
         scene.append(scoreLabel);
@@ -49,63 +59,27 @@ function main(param) {
             scene: scene,
             font: font,
             fontSize: 16,
-            text: "1234567890",
-            x: 0,   // 左端に配置
+            text: "50.0",
+            x: 0, 
             y: 0
         });
         scene.append(timerLabel);
-        var squareme = new g.FilledRect({//自機
-            scene: scene,
-            cssColor: "black",
-            x: (g.game.width - playersize) / 2,
-            y: (g.game.height - playersize) / 2,
-            width: playersize,
-            height: playersize,
-        });
-        scene.append(squareme);
-        scene.pointMoveCapture.add(function (ev) {//自機更新
-            squareme.x += ev.prevDelta.x;
-            if (squareme.x < 0) {
-                squareme.x = 0;
-            }
-            if (squareme.x > g.game.width - squareme.width) {
-                squareme.x = g.game.width - squareme.width;
-            }
-            squareme.y += ev.prevDelta.y;
-            if (squareme.y < 0) {
-                squareme.y = 0;
-            }
-            if (squareme.y > g.game.height - squareme.height) {
-                squareme.y = g.game.height - squareme.height;
-            }
-            squareme.modified();
-        });
-        scene.update.add(function (e) {//四角形の生成
-            if (frameCount >= (g.game.fps * 40) && (frameCount % 5) == 0) {
-                generatesquarer();
-            } else
-                if ((frameCount % 10) == 0) {
-                    generatesquarer();
-                }
-            if (frameCount >= (g.game.fps * 10) && (frameCount % 20) == 0) {
-                generatesquareb();
-            }
-            if (frameCount >= (g.game.fps * 30) && (frameCount % 60) == 0) {
-                generatesquareg();
-            }
-        });
         scene.update.add(function (e) {//接触判定と減点
-            for (var i = 0; i < scene.children.length - 1; i++) {//全オブジェクトを探索
-                if (haikei !== scene.children[i] && squareme !== scene.children[i] && scoreLabel !== scene.children[i] && timerLabel !== scene.children[i] && g.Collision.intersectAreas(squareme, scene.children[i]) && scene.children[i].opacity == 1.0) {//接触判定
-                    if (Math.floor(gameTimeLimit * 10 - frameCount / g.game.fps * 10) > 0) {
+            squareme.opacity=1;
+            squareme.modified();
+            for (var i = 0; i < scene.children.length; i++) {//全オブジェクトを探索
+                if (haikei !== scene.children[i] && squareme !== scene.children[i] && scoreLabel !== scene.children[i] && timerLabel !== scene.children[i] && rule !== scene.children[i]&& buttonretry !== scene.children[i]&& g.Collision.intersectAreas(squareme, scene.children[i]) && scene.children[i].opacity == 1.0) {//接触判定
+                    if (Math.floor(time * 10 - (timeall-ruletime*g.game.fps) / g.game.fps * 10) > 0) {
                         if ("red" == scene.children[i].cssColor) {//色を判定
                             g.game.vars.gameState.score -= 70;
                         } else if ("blue" == scene.children[i].cssColor) {
                             g.game.vars.gameState.score -= 300;
                         } else if ("green" == scene.children[i].cssColor) {
                             g.game.vars.gameState.score -= 1000;
-                        }
+                        }//満点は15000
                     }
+                    squareme.opacity=0.5;
+                    squareme.modified();
                     scene.children[i].destroy();
                     i--;
                 }
@@ -126,39 +100,61 @@ function main(param) {
                 scoreLabel.text = "" + g.game.vars.gameState.score;
             }
             scoreLabel.invalidate();//スコア表示更新
-            if (Math.floor(gameTimeLimit * 10 - frameCount / g.game.fps * 10) <= 0) {
-                scene.update.remove(updateStatus); //時間切れでupdateStatusを消す
-            }
-            ++frameCount;
             updateTimerLabel();
         }
-        scene.update.add(updateStatus);
         function updateTimerLabel() {//タイマー更新
-            var s = Math.floor(gameTimeLimit * 10 - frameCount / g.game.fps * 10);
-            var text = Math.floor(gameTimeLimit * 10 - frameCount / g.game.fps * 10) / 10 + (s % 10 === 0 ? ".0" : "");
+            var s = Math.floor(time * 10 - (timeall-ruletime*g.game.fps) / g.game.fps * 10);
+            var text = Math.floor(time * 10 - (timeall-ruletime*g.game.fps) / g.game.fps * 10) / 10 + (s % 10 === 0 ? ".0" : "");
             if (timerLabel.text != text) {//表示と実際の時間が違うときに更新
                 timerLabel.text = text;
                 timerLabel.invalidate();
             }
         }
+        var squareme = new g.FilledRect({//自機
+            scene: scene,
+            cssColor: "black",
+            x: (g.game.width - playersize) / 2,
+            y: (g.game.height - playersize) / 2,
+            width: playersize,
+            height: playersize,
+        });
+        scene.append(squareme);
+        scene.append(rule);
+        scene.pointMoveCapture.add(function (ev) {//自機更新
+            squareme.x += ev.prevDelta.x;
+            if (squareme.x < 0) {
+                squareme.x = 0;
+            }
+            if (squareme.x > g.game.width - squareme.width) {
+                squareme.x = g.game.width - squareme.width;
+            }
+            squareme.y += ev.prevDelta.y;
+            if (squareme.y < 0) {
+                squareme.y = 0;
+            }
+            if (squareme.y > g.game.height - squareme.height) {
+                squareme.y = g.game.height - squareme.height;
+            }
+            squareme.modified();
+        });
         // 敵情報
         function generatesquarer() {//赤四角(バウンド)
-            var edge = g.game.random.get(16, 32);
+            var edge = param.random.get(16, 32);
             var squarer = new g.FilledRect({
                 scene: scene,
                 cssColor: "red",
                 width: edge,
                 height: edge,
-                x: g.game.random.get(0, 1) * (g.game.width) - edge / 2,
-                y: g.game.random.get(0, 1) * (g.game.height) - edge / 2,
+                x: param.random.get(0, 1) * (g.game.width) - edge / 2,
+                y: param.random.get(0, 1) * (g.game.height) - edge / 2,
                 opacity: 1.0
             });
             squarer.tag = {
                 counter: 0,//四角が生成されてからのフレーム数
                 xplus: "true",
                 yplus: "true",
-                xyspeed: g.game.random.get(3, 8),//速度
-                xyangle: g.game.random.get(1, 89) / 90 * Math.PI / 2,//角度
+                xyspeed: param.random.get(3, 8),//速度
+                xyangle: param.random.get(1, 89) / 90 * Math.PI / 2,//角度
             };
             squarer.update.add(function () {
                 squarer.tag.counter++;
@@ -198,20 +194,20 @@ function main(param) {
             scene.append(squarer);//追加・描画
         }
         function generatesquareb() {//青四角(ループ)
-            var edge = g.game.random.get(16, 32);
+            var edge = param.random.get(16, 32);
             var squareb = new g.FilledRect({
                 scene: scene,
                 cssColor: "blue",
                 width: edge,
                 height: edge,
-                x: g.game.random.get(0, 1) * (g.game.width) - edge / 2,
-                y: g.game.random.get(0, 1) * (g.game.height) - edge / 2,
+                x: param.random.get(0, 1) * (g.game.width) - edge / 2,
+                y: param.random.get(0, 1) * (g.game.height) - edge / 2,
                 opacity: 1
             });
             squareb.tag = {
                 counter: 0,//四角が生成されてからのフレーム数
-                xyspeed: g.game.random.get(3, 5),//速度
-                xyangle: (g.game.random.get(1, 89) / 180 + g.game.random.get(0, 3) / 2) * Math.PI//角度
+                xyspeed: param.random.get(3, 5),//速度
+                xyangle: (param.random.get(1, 89) / 180 + param.random.get(0, 3) / 2) * Math.PI//角度
             };
             squareb.update.add(function () {
                 squareb.tag.counter++;
@@ -231,19 +227,19 @@ function main(param) {
             scene.append(squareb);//追加・描画
         }
         function generatesquareg() {//緑四角(追尾)
-            var edge = g.game.random.get(16, 32);
+            var edge = param.random.get(16, 32);
             var squareg = new g.FilledRect({
                 scene: scene,
                 cssColor: "green",
                 width: edge,
                 height: edge,
-                x: g.game.random.get(0, 1) * (g.game.width) - edge / 2,
-                y: g.game.random.get(0, 1) * (g.game.height) - edge / 2,
+                x: param.random.get(0, 1) * (g.game.width) - edge / 2,
+                y: param.random.get(0, 1) * (g.game.height) - edge / 2,
                 opacity: 1
             });
             squareg.tag = {
                 counter: 0,//四角が生成されてからのフレーム数
-                xyspeed: g.game.random.get(2, 5),//速度
+                xyspeed: param.random.get(2, 5),//速度
                 gx: squareg.x,//現在の座標
                 gy: squareg.y
             };
@@ -266,18 +262,100 @@ function main(param) {
             });
             scene.append(squareg);//追加・描画
         }
+        //開始時に何か置いとかないといけない              
+        if (param.isAtsumaru) {//Retryボタン(param.isAtsumaru)
+            var buttonretry = new g.FilledRect({//ボタン(retry)
+                scene: scene,
+                cssColor: "black",
+                x: -108 + ((g.game.width)),
+                y: 320,
+                width: 96,
+                height: 32,
+                touchable: true,
+            });
+            var buttonretry2 = new g.FilledRect({//ボタン(retry)
+                scene: scene,
+                cssColor: "white",
+                x: 3,
+                y: 3,
+                width: 90,
+                height: 26,
+            });
+            var textretry = new g.Label({//文字(retry)
+                scene: scene,
+                font: font,
+                fontSize: 16,
+                text: "Retry",
+                tag: { sum: 0 },
+                x: 8,
+                y: 6,
+            });
+            buttonretry.append(buttonretry2);
+            buttonretry.append(textretry);
+            scene.append(buttonretry);            
+            buttonretry.pointDown.add(function () {//ボタン(retry)推したとき
+                squareme.x=(g.game.width - playersize) / 2,
+                squareme.y=(g.game.height - playersize) / 2,
+                g.game.vars.gameState.score = 0;
+                for (var i = 0; i < scene.children.length; i++) {//全オブジェクトを探索
+                    if (haikei !== scene.children[i] && squareme !== scene.children[i] && scoreLabel !== scene.children[i] && timerLabel !== scene.children[i] && rule !== scene.children[i]&& buttonretry !== scene.children[i]) {//接触判定
+                        scene.children[i].destroy();
+                        i--;
+                    }
+                }
+                timeall= ((ruletime)*g.game.fps-1)              
+                window.RPGAtsumaru.globalServerVariable.triggerCall(2237);
+            });
+            buttonretry.update.add(function () {
+                if(timeall>=((time+ruletime)*g.game.fps)+10){
+                    buttonretry.show();  
+                }else{
+                    buttonretry.hide();   
+                }
+            });
+            
+        }      
+        var updateHandler = function () {
+            if(timeall>=((ruletime)*g.game.fps-10)){
+                rule.opacity = 0;
+            }
+            if ((timeall) >= (ruletime * g.game.fps) && (timeall) <= ((ruletime + time) * g.game.fps)) {
+                if ((timeall - ruletime * g.game.fps) >= (g.game.fps * 40) && ((timeall - ruletime * g.game.fps) % 5) == 0) {
+                    generatesquarer();
+                } else if (((timeall - ruletime * g.game.fps) % 10) == 0) {
+                    generatesquarer();
+                }
+                if ((timeall - ruletime * g.game.fps) >= (g.game.fps * 10) && ((timeall - ruletime * g.game.fps) % 20) == 0) {
+                    generatesquareb();
+                }
+                if ((timeall - ruletime * g.game.fps) >= (g.game.fps * 30) && ((timeall - ruletime * g.game.fps) % 60) == 0) {
+                    generatesquareg();
+                }
+            }      
+            // カウントダウン処理
+            if(timeall<=(ruletime*g.game.fps)){//開始前
+                timerLabel.text = ""+time+".0";
+            } else if (timeall > ((time + ruletime) * g.game.fps)) {//終了時
+                timerLabel.text = ""+"0.0";
+            } else {
+                updateStatus()
+            }
+            timerLabel.invalidate();
+            scoreLabel.invalidate();
+            if (timeall==((time+ruletime)*g.game.fps)+1) {
+                // RPGアツマール環境であればランキングを表示します
+                if (param.isAtsumaru) {
+                    var boardId_1 = 1;
+                    window.RPGAtsumaru.experimental.scoreboards.setRecord(boardId_1, g.game.vars.gameState.score).then(function () {
+                        window.RPGAtsumaru.experimental.scoreboards.display(boardId_1);
+                    });
+                }
+            }
+            timeall++;
+        };
+        scene.update.add(updateHandler);
         // ここまでゲーム内容を記述します
-        scene.update.add(function () {
-            if (frameCount2==(gameTimeLimit * g.game.fps)&&atumar == 1&&window !== undefined && window.RPGAtsumaru) {
-                window.RPGAtsumaru.experimental.scoreboards.setRecord(1, g.game.vars.gameState.score);
-            }
-            if (frameCount2==(gameTimeLimit * g.game.fps+30)&&atumar == 1&&window !== undefined && window.RPGAtsumaru) {
-                window.RPGAtsumaru.experimental.scoreboards.display(1);
-            }
-            frameCount2++;
-        });
     });
     g.game.pushScene(scene);
-
 }
-module.exports = main;
+exports.main = main;
